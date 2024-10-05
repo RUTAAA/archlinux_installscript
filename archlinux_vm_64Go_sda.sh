c51c5681132e5e2a4497cba151aeaf2416baaee7
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-05 12:04:14 +0200

    Update archlinux_vm_64Go_sda.sh

--- archlinux_vm_64Go_sda.sh
+++ archlinux_vm_64Go_sda.sh
@@ -16,27 +16,24 @@ pacstrap -K /mnt base linux linux-firmware base-devel nano grub efibootmgr
 echo "fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "Changement de racine"
-arch-chroot /mnt
-
 echo "Langue et layout"
-sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /etc/locale.gen
-locale-gen
-touch /etc/vconsole.conf
-echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
-touch /etc/vconsole.conf
-echo "KEYMAP=fr" > /etc/vconsole.conf
+sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+touch /mnt/etc/vconsole.conf
+echo "LANG=fr_FR.UTF-8" > /mnt/etc/locale.conf
+touch /mnt/etc/vconsole.conf
+echo "KEYMAP=fr" > /mnt/etc/vconsole.conf
 
 echo "Hostname"
-touch /etc/hostname
-echo "archlinux" > /etc/hostname
+touch /mnt/etc/hostname
+echo "archlinux" > /mnt/etc/hostname
 
 echo "Date et heure"
-ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc
+ln -sf /mnt/usr/share/zoneinfo/Europe/Paris /mnt/etc/localtime
+arch-chroot /mnt hwclock --systohc
 
 echo "GRUB bootloader"
-grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Terminé!"
