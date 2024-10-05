ac6923f15da7e1d2b202ca7c6116d071d00dd486
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-05 19:49:45 +0200

    Update archlinux_vm_64Go_sda.sh

--- archlinux_vm_64Go_sda.sh
+++ archlinux_vm_64Go_sda.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "Mise à jour"
+echo "User to create: "
+read userneame
+echo "Password of ${username}:"
+read password
+
+
+echo "Update"
 pacman -Sy
 
-echo "Partitionnement, formatage, montage"
+echo "Disk"
 sfdisk /dev/sda < sda.dump
 mkfs.ext4 /dev/sda2
 mkfs.fat -F 32 /dev/sda1
@@ -11,12 +17,12 @@ mount /dev/sda2 /mnt
 mount --mkdir /dev/sda1 /mnt/boot
 
 echo "Installation"
-pacstrap -K /mnt base linux linux-firmware base-devel nano grub efibootmgr
+pacstrap -K /mnt base linux linux-firmware grub efibootmgr base-devel nano
 
 echo "fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "Langue et layout"
+echo "Localization"
 sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 touch /mnt/etc/vconsole.conf
@@ -28,12 +34,12 @@ echo "Hostname"
 touch /mnt/etc/hostname
 echo "archlinux" > /mnt/etc/hostname
 
-echo "Date et heure"
+echo "Datetime"
 ln -sf /mnt/usr/share/zoneinfo/Europe/Paris /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
 
-echo "Mot de passe root"
-arch-chroot /mnt echo "password" | passwd --stdin
+echo "User creation"
+arch-chroot /mnt useradd $username --groups wheel --create-home --password $password --shell /bin/bash
 
 echo "GRUB bootloader"
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
