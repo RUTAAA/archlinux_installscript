747d9aef51bae51d55e4ab2814955fa8aba49b87
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-06 15:20:33 +0200

    Update archlinux_vm_64Go_sda.sh

--- archlinux_vm_64Go_sda.sh
+++ archlinux_vm_64Go_sda.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-read -p "User to create: " userneame
-read -p "Password of this user: " password
+read -s -p "Root password in the new installation: " password
+echo
 
-echo "Update"
+echo "\n\n\nUpdate"
 pacman -Sy
 
-echo "Disk"
+echo "\n\n\nDisk"
 sfdisk /dev/sda < sda.dump
 mkfs.ext4 /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 mount /dev/sda2 /mnt
 mount --mkdir /dev/sda1 /mnt/boot
 
-echo "Installation"
+echo "\n\n\nInstallation"
 pacstrap -K /mnt base linux linux-firmware grub efibootmgr base-devel nano
 
-echo "fstab"
+echo "\n\n\nfstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "Localization"
+echo "\n\n\nLocalization"
 sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 touch /mnt/etc/vconsole.conf
@@ -27,19 +27,19 @@ echo "LANG=fr_FR.UTF-8" > /mnt/etc/locale.conf
 touch /mnt/etc/vconsole.conf
 echo "KEYMAP=fr" > /mnt/etc/vconsole.conf
 
-echo "Hostname"
+echo "\n\n\nHostname"
 touch /mnt/etc/hostname
 echo "archlinux" > /mnt/etc/hostname
 
-echo "Datetime"
+echo "\n\n\nDatetime"
 ln -sf /mnt/usr/share/zoneinfo/Europe/Paris /mnt/etc/localtime
 arch-chroot /mnt hwclock --systohc
 
-echo "Root password"
-arch-chroot /mnt echo $password | passwd $username --stdin
+echo "\n\n\nRoot password"
+arch-chroot /mnt echo $password | passwd --stdin
 
-echo "GRUB bootloader"
+echo "\n\n\nGRUB bootloader"
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Terminé!"
+echo "\n\n\nTerminé!"
