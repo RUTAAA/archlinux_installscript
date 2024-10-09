cd9aa4e17bf564d32068d98498b760630230b191
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-09 14:42:45 +0200

    Update chroot_script.sh

--- chroot_script.sh
+++ chroot_script.sh
@@ -1,1 +1,40 @@
+echo
+echo
+echo
+echo "Localization"
+sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /etc/locale.gen
+locale-gen
+touch /etc/vconsole.conf
+echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
+touch /etc/vconsole.conf
+echo "KEYMAP=fr" > /etc/vconsole.conf
 
+echo
+echo
+echo
+echo "Hostname"
+touch /etc/hostname
+echo "archlinux" > /etc/hostname
+
+echo
+echo
+echo
+echo "Datetime"
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+echo
+echo
+echo
+echo "User and password"
+echo $root_password | passwd --stdin
+useradd --groups wheel --create-home --shell /bin/bash
+echo $password | passwd --stdin $username
+sed -i "s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers
+
+echo
+echo
+echo
+echo "GRUB bootloader"
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
