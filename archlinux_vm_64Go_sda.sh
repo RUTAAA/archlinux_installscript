dfa296a4f2bf15d243ce0cee96f0846bc9c4f5ae
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-09 14:45:56 +0200

    Update archlinux_vm_64Go_sda.sh

--- archlinux_vm_64Go_sda.sh
+++ archlinux_vm_64Go_sda.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-read -p "New username: " username
-echo
-
-read -s -p "New user password: " password
-echo
-
-read -s -p "New root password: " root_password
-echo
-
-
-echo
-echo
-echo
 echo "Update"
 pacman -Sy
 
@@ -41,43 +28,8 @@ genfstab -U /mnt >> /mnt/etc/fstab
 echo
 echo
 echo
-echo "Localization"
-sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
-touch /mnt/etc/vconsole.conf
-echo "LANG=fr_FR.UTF-8" > /mnt/etc/locale.conf
-touch /mnt/etc/vconsole.conf
-echo "KEYMAP=fr" > /mnt/etc/vconsole.conf
-
-echo
-echo
-echo
-echo "Hostname"
-touch /mnt/etc/hostname
-echo "archlinux" > /mnt/etc/hostname
-
-echo
-echo
-echo
-echo "Datetime"
-ln -sf /mnt/usr/share/zoneinfo/Europe/Paris /mnt/etc/localtime
-arch-chroot /mnt hwclock --systohc
-
-echo
-echo
-echo
-echo "User and password"
-arch-chroot /mnt (echo $root_password | passwd --stdin)
-arch-chroot /mnt useradd --groups wheel --create-home --shell /bin/bash
-arch-chroot /mnt (echo $password | passwd --stdin $username)
-
-echo
-echo
-echo
-echo "GRUB bootloader"
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-sed -i "s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/"
+echo "Change root"
+arch-chroot /mnt ./chroot_script.sh
 
 echo
 echo
