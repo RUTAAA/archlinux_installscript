776ef98272f17bc86996a91564666910c2abe2ff
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-10 16:24:40 +0200

    Update chroot_script.sh

--- chroot_script.sh
+++ chroot_script.sh
@@ -1,15 +1,4 @@
-read -p "New username: " username
-read -s -p "New user password: " password
-echo
-read -s -p "New root password: " root_password
-echo
-
-echo
-echo
-echoecho
-echo
-echo
-echo "Localization"
+printf "\n\n\nLocalization"
 sed -i "s/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/" /etc/locale.gen
 locale-gen
 touch /etc/vconsole.conf
@@ -17,32 +6,20 @@ echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
 touch /etc/vconsole.conf
 echo "KEYMAP=fr" > /etc/vconsole.conf
 
-echo
-echo
-echo
-echo "Hostname"
+printf "\n\n\nHostname"
 touch /etc/hostname
 echo "archlinux" > /etc/hostname
 
-echo
-echo
-echo
-echo "Datetime"
+printf "\n\n\n"Datetime"
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 
-echo
-echo
-echo
-echo "User and password"
-echo $root_password | passwd --stdin
-useradd --groups wheel --create-home --shell /bin/bash $username
-echo $password | passwd --stdin $username
+printf "\n\n\nUser and password"
+echo $1 | passwd --stdin
+useradd --groups wheel --create-home --shell /bin/bash $2
+echo $3 | passwd --stdin $2
 sed -i "s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/" /etc/sudoers
 
-echo
-echo
-echo
-echo "GRUB bootloader"
+printf "\n\n\nGRUB bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
