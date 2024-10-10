6d7dbee36fd8b31e4568f863784b642a7e743d62
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-10 15:52:51 +0200

    Update archlinux_vm_64Go_sda.sh

--- archlinux_vm_64Go_sda.sh
+++ archlinux_vm_64Go_sda.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
 
-echo "Update"
+read -s -p "Password for new root" root_password
+echo
+read -p "Username for new user" username
+read -s -p "Password for new user" user_password
+echo
+read -p "Additional packages to install: " packages
+
+printf "\n\n\nUpdate"
 pacman -Sy
 
-echo
-echo
-echo
-echo "Disk"
+printf "\n\n\nDisk"
 sfdisk /dev/sda < sda.dump
 mkfs.ext4 /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 mount /dev/sda2 /mnt
 mount --mkdir /dev/sda1 /mnt/boot
 
-echo
-echo
-echo
-echo "Installation"
-pacstrap -K /mnt base linux linux-firmware grub efibootmgr base-devel nano
+printf "\n\n\nInstallation"
+pacstrap -K /mnt base linux linux-firmware grub efibootmgr base-devel nano $packages
 
-echo
-echo
-echo
-echo "fstab"
+printf "\n\n\nfstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo
-echo
-echo
-echo "Change root"
+printf "\n\n\nChange root"
 cp ./chroot_script.sh /mnt/chroot_script.sh
-arch-chroot /mnt ./chroot_script.sh
+arch-chroot /mnt ./chroot_script.sh $root_password $username $user_password
 rm /mnt/chroot_script.sh
 
-echo
-echo
-echo
-echo "Terminé!"
+printf "\n\n\nTerminé!"
