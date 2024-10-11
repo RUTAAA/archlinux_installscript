9546b2b47f9698822ca3daa4f1a0f68c2cadd524
RUTAAA <82164338+RUTAAA@users.noreply.github.com>
2024-10-11 12:21:23 +0200

    Update ricing.sh

--- ricing.sh
+++ ricing.sh
@@ -1,30 +1,31 @@
-printf "\n\n\nRicing installations"
+printf "\n\n\nRicing installations\n"
 pacman -S --noconfirm xorg ly bspwm sxhkd picom feh polybar rofi wezterm firefox zsh ttf-nerd-fonts-symbols-mono
 
-printf "\n\n\nX11 - Display Server"
-localectl --no-convert set-x11-keymap fr pc105 azerty
+printf "\n\n\nX11 - Display Server\n"
+touch /etc/X11/xorg.conf.d/00-keyboard.conf
+echo "Section \"InputClass\"\n\tIdentifier \"system-keyboard\"\n\tMatchIsKeyboard \"on\"\n\tOption \"XkbLayout\" \"fr\"\n\tOption \"XkbModel\" \"pc105\"\n\tOption \"XkbVariant\" \"azerty\"\nEndSection" > /etc/X11/xorg.conf.d/00-keyboard.conf
 cp /etc/X11/xinit/xinitrc /home/$1/.xinitrc
 head -n -5 /home/$1/.xinitrc > /home/$1/.xinitrc
 echo "exec bspwm" >> /home/$1/.xinitrc
 
-printf "\n\n\nLy - Display Manager"
+printf "\n\n\nLy - Display Manager\n"
 systemctl enable ly.service
 
-printf "\n\n\nbspwm - Window Manager"
+printf "\n\n\nbspwm - Window Manager\n"
 install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc /home/$1/.config/bspwm/bspwmrc
 
-printf "\n\n\nsxhkd - Hotkey Deamon"
+printf "\n\n\nsxhkd - Hotkey Deamon\n"
 install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc /home/$1/.config/sxhkd/sxhkdrc
 
-printf "\n\n\npicom - Compositor"
+printf "\n\n\npicom - Compositor\n"
 echo "picom &" >> /home/$1/.config/bspwm/bspwmrc
 
-printf "\n\n\nfeh - Desktop Wallpaper"
+printf "\n\n\nfeh - Desktop Wallpaper\n"
 mkdir /home/$1/.config/wallpapers
 curl https://w.wallhaven.cc/full/jx/wallhaven-jxogjw.jpg --output /home/$1/.config/wallpapers/wallpaper.jpg
 echo "feh --bg-fill /home/$1/.config/wallpapers/wallpaper.jpg &" >> /home/$1/.config/bspwm/bspwmrc
 
-printf "\n\n\nPolybar - Status Bar"
+printf "\n\n\nPolybar - Status Bar\n"
 mkdir /home/$1/.config/polybar
 cp /etc/polybar/config.ini /home/$1/.config/polybar/config.ini
 touch /home/$1/.config/polybar/launch.sh
@@ -40,7 +41,7 @@ touch /home/$1/.config/wezterm/wezterm.lua
 printf "local wezterm = require 'wezterm'\nlocal config = wezterm.config_builder()\nreturn config" > /home/$1/.config/wezterm/wezterm.lua
 sed -i "s/urxvt/wezterm/" /home/$1/.config/sxhkd/sxhkdrc
 
-printf "\n\n\nFirefox - Internet Browser"
+printf "\n\n\nFirefox - Internet Browser\n"
 touch /home/$1/.config/sxhkd/sxhkdrc_new
 head -n 11 /home/$1/.config/sxhkd/sxhkdrc > /home/$1/.config/sxhkd/sxhkdrc_new
 echo -e "\n# internet browser\nsuper + b\n\tfirefox" >> /home/$1/.config/sxhkd/sxhkdrc_new
